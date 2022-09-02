81523df0d3a5638a6b910075b1bc955e6ff871b2
Ogu1208 <kdasunb@naver.com>
2022-09-03 00:46:01 +0900

    조건문, 반복문, 다차원 배열

--- Part 01. Java 기초/src/Java_04/Main.java
+++ Part 01. Java 기초/src/Java_04/Main.java
@@ -7,17 +7,53 @@ public class Main {
 
 //      1. 조건문 - if
         System.out.println("== if ==");
+        int watwerTempaerature = 100;
 
+        if (watwerTempaerature >= 100) {
+            System.out.println("물이 끓습니다.");
+        } else {
+            System.out.println("물을 끓이는 중입니다.");
+        }
+
+        int score = 60;
+        char grade = 0;
+
+        if (score >= 90) {
+            grade = 'A';
+        } else if (score >= 80) {
+            grade = 'B';
+        } else if (score >= 70) {
+            grade = 'C';
+        } else {
+            grade = 'F';
+        }
+        System.out.println("grade = " + grade);
 
 
 //      2. 조건문 - switch
         System.out.println("== switch ==");
+        String fruit = "apple";
 
+        switch (fruit) {
+            case "apple":
+                System.out.println(fruit + "은 5000원 입니다.");
+                break;
+            case "blueberry":
+                System.out.println(fruit + "은 10000원 입니다.");
+                break;
+            default:
+                System.out.println("해당 과일이 없습니다.");
+                break;
+        }
 
 
 //      Q1. number의 값이 홀수인지 짝수인지 판단하는 코드를 작성하세요.
         int number = 5;
-
+        if (number % 2 == 0) {
+            System.out.println("짝수입니다.");
+        } else {
+            System.out.println("홀수입니다.");
+        }
 
 
 //      Q2. 아래 주석은 위의 실습에서 진행한 score에 따라 grade를 출력하는 코드이다.
@@ -35,6 +71,25 @@ public class Main {
 //        }
 //        System.out.println("grade = " + grade);
 
+        score = 85;
+        grade = 0;
+
+        switch(score / 10) {
+            case 10:
+            case 9:
+                grade = 'A';
+                break;
+            case 8:
+                grade = 'B';
+                break;
+            case 7:
+                grade = 'C';
+                break;
+            default:
+                grade = 'F';
+                break;
+        }
+        System.out.println("grade = " + grade);
 
     }
 }
