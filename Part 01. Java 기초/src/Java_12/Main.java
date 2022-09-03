1f21ed63c665cc07c3c38510e8410b391af4aa96
Ogu1208 <kdasunb@naver.com>
2022-09-03 17:13:48 +0900

    콘솔 입출력

--- Part 01. Java 기초/src/Java_12/Main.java
+++ Part 01. Java 기초/src/Java_12/Main.java
@@ -1,19 +1,56 @@
 package Java_12;
 // Java 프로그래밍 - 내부 클래스
 
+// 내부 클래스 구조
+class Outer {
+    public void print() {
+        System.out.println("Outer.print");
+    }
+
+    class Inner {
+        public void innerPrint() {
+            Outer.this.print();
+        }
+    }
+
+    static class InnerStaticClass{
+        void innerPrint() {
+//            Outer.this.print();    // static 클래스가 아니라서 바로 메모리에 올라가지 않음
+        }
+    }
+}
+
+abstract  class Person {
+    public abstract void printInfo();
+}
+
+class Student extends Person {
+    public void printInfo() {
+        System.out.println("Student.printInfo");
+    }
+}
+
 public class Main {
 
     public static void main(String[] args) {
 
 //      외부 클래스
+        Outer o1 = new Outer();
 
 //      내부 클래스 - 인스턴스
+        Outer.Inner i1 = new Outer().new Inner();   // 외부 객체->내부객체를 만들어서 접근해야함
 
-//      내부 클래스 - 정적
+//      내부 클래스 - 정적 (static)
+        Outer.InnerStaticClass is1 = new Outer.InnerStaticClass();
+        // static이기 때문에 Outer의 객체를 생성하지 않고 .으로 불러올 수 있다. (클래스 이름으로 접근)
 
-//      익명 클래스
-
-
+//      익명 클래스   (anonymous)
+        Person p1 = new Person() {
+            @Override
+            public void printInfo() {
+                System.out.println("Main.printInfo");
+            }
+        };
     }
 
 }
--- Part 01. Java 기초/src/Java_13_1/Main.java
+++ Part 01. Java 기초/src/Java_13_1/Main.java
@@ -1,21 +1,39 @@
 package Java_13_1;
 // Java 프로그래밍 - 입출력_1
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Main {
 
     public static void referInputStream() throws IOException {
 //      System.in
         System.out.println("== System.in ==");
+//        System.out.print("입력 :");
+//        int a = System.in.read() - '0';    // 문자를 읽어서 숫자로 변환, 엔터키까지 포함
+//        System.out.println("a = " + a);
+//        // 입력스트림에 남아있는 갯수 만큼 byte 형태로 읽어서 소진시킴(읽음)
+//        System.in.read(new byte[System.in.available()]);  // 남은 엔터키를 없애줌
 
 
 //      InputStreamReader
+//      여러개의 데이터를 받음
         System.out.println("== InputStreamReader ==");
+//        InputStreamReader reader = new InputStreamReader(System.in);
+//        char[] c = new char[3];
+//        System.out.println("입력: ");
+//        reader.read(c);   // 데이터를 읽어서 배열에 넣어준다
+//        System.out.println(c);
 
 
 //      BufferedReader
         System.out.println("== BufferedReader ==");
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        System.out.print("입력: ");
+//        String s1 = br.readLine();
+//        System.out.println("s1 = " + s1);
 
     }
 
@@ -27,13 +45,59 @@ public class Main {
 
 //      1-2. Scanner
         System.out.println("== Scanner ==");
+        Scanner sc = new Scanner(System.in);
+//        System.out.print("입력: ");
+//        System.out.println(sc.next());   // 하나를 받아옴
+//        sc.nextLine();  // 엔터키 소진
 
+//        System.out.print("입력2: ");
+//        System.out.println(sc.nextInt());   // 숫자만 입력 가능
+//        sc.nextLine();
+
+//        System.out.print("입력3: ");
+//        System.out.println(sc.nextLine());
 
 //      참고) 정수, 문자열 변환
-
+        int num = Integer.parseInt("12345");  // 문자열 -> 정수
+        String str = Integer.toString(12345);  //  정수 -> 문자열
 
 //      2. 출력
         System.out.println("== 출력 ==");
+        System.out.println("Hello");
+        System.out.println("World!");
 
+        System.out.print("Hello ");
+        System.out.print("World!");
+        System.out.println();
+
+        System.out.printf("Hello ");
+        System.out.printf("World!");
+        System.out.println();
+
+        String s = "자바";
+        int number = 3;
+
+        System.out.println(s + "는 언어 선호도 " + number + "위 입니다.");
+        System.out.printf("%s는 언어 선호도 %d위 입니다.\n", s, number);
+
+        System.out.printf("%d\n", 10);  // 정수
+        System.out.printf("%o\n", 10);  // 8진수
+        System.out.printf("%x\n", 10);  // 16진수
+
+        System.out.printf("%f\n", 5.2f);   // 실수
+
+        System.out.printf("%c\n", 'A');   // 문자
+        System.out.printf("%s\n", "안녕하세요");   // 문자열
+
+//        줄맞춤, 공백
+        System.out.printf("%5d\n", 123);
+        System.out.printf("%5d\n", 1234);
+        System.out.printf("%5d\n", 12345);
+
+        System.out.printf("%-5d\n", 123);
+        System.out.printf("%-5d\n", 1234);
+        System.out.printf("%-5d\n", 12345);
+
+        System.out.printf("%.2f\n", 1.12645123f);   // 2번째 자리까지, 반올림 출력
     }
 }
