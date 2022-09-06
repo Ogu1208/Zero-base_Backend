907648af5a4a16f1065c535312da969d1f47b5fa
Ogu1208 <kdasunb@naver.com>
2022-09-07 00:57:29 +0900

    자바 기초 연습 문제 풀이 끝

--- Part 01. Java 기초/src/Java_18_3/src/Practice4.java
+++ Part 01. Java 기초/src/Java_18_3/src/Practice4.java
@@ -4,16 +4,51 @@ import java.util.ArrayList;
 
 public class Practice4 {
     public static ArrayList<Integer> solution(int[][] matrix) {
+        if (matrix == null || matrix.length == 0) {
+            return null;
+        }
 
-        return null;
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        int rowStart = 0;
+        int rowEnd = matrix.length - 1;
+        int colStart = 0;
+        int colEnd = matrix[0].length - 1;
+
+        while(rowStart <= rowEnd && colStart <= colEnd) {
+            for (int i = colStart; i <= colEnd; i++) { // 첫줄 오른쪽 방향
+                result.add(matrix[rowStart][i]);
+            }
+            rowStart++;
+
+            for (int i = rowStart; i <= rowEnd; i++) {   // colEnd 오른쪽 아랫방향
+                result.add(matrix[i][colEnd]);
+            }
+            colEnd--;
+
+            if(rowStart <= rowEnd) {
+                for (int i = colEnd; i >= colStart; i--) {  // 마지막줄 왼쪽방향
+                    result.add(matrix[rowEnd][i]);
+                }
+            }
+            rowEnd--;
+
+            if(colStart <= colEnd) {
+                for (int i = rowEnd; i >= rowStart; i--) {   // 왼쪽 위로 올라가는 방향
+                    result.add(matrix[i][colStart]);
+                }
+            }
+            colStart++;
+        }
+
+        return result;
     }
-    
+
     public static void main(String[] args) {
         // Test code
         int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
         System.out.println((solution(matrix)));
 
-        matrix = new int[][]{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10 ,11 ,12}};
+        matrix = new int[][]{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
         System.out.println((solution(matrix)));
     }
 }
--- Part 01. Java 기초/src/Java_18_3/src/Practice5.java
+++ Part 01. Java 기초/src/Java_18_3/src/Practice5.java
@@ -2,9 +2,37 @@ package Java_18_3.src;
 
 public class Practice5 {
     public static int solution(int[] height) {
-        return 0;
+        if (height == null || height.length == 0) {
+            return 0;
+        }
+
+        int left = 0;
+        int right = height.length - 1;
+        int leftMax = 0;
+        int rightMax = 0;
+        int result = 0;
+
+        while (left < right) {
+            if (height[left] < height[right]) {
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];
+                } else {
+                    result += leftMax - height[left];
+                }
+                left++;
+            } else {
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];
+                } else {
+                    result += rightMax - height[right];
+                }
+                right--;
+            }
+        }
+
+        return result;
     }
-    
+
     public static void main(String[] args) {
         // Test code
         int[] height = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
