c5c81123115380de6a031ceb42b6ce3ed6bf6720
Ogu1208 <kdasunb@naver.com>
2022-09-16 23:45:40 +0900

    변수 상수

---
+++ Part 01-3. Java 핵심 복습/exam-03/Sam08.java
@@ -0,0 +1,16 @@
+public class Sam08 {
+  public static void main(String[] args) {
+    int i1 = 10;
+    long l1 = 0x7fff_ffff_ffff_ffffL;
+    long l2 = 20L;
+    char c1 = '가';
+    
+    
+
+    double d1 = 3.1E2;
+    double d2 = 0x3.1P2;
+
+    System.out.println(d1);
+    System.out.println(d2);
+  }
+}
