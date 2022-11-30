02e9143a3c5ca7c4fc59722d23f3a1ebadbf6260
Ogu1208 <kdasunb@naver.com>
2022-11-30 22:27:44 +0900

    스프링 HTTP 요청 파라미터 전송 : Get, Delete, Post, Put, Patch PathVariable, query-params, RequestsBody, RequestsHeader

--- Part 07 스프링 프레임워크/WebSample/src/main/java/com/example/websample/controller/SampleController.java
+++ Part 07 스프링 프레임워크/WebSample/src/main/java/com/example/websample/controller/SampleController.java
@@ -1,5 +1,6 @@
 package com.example.websample.controller;
 
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
@@ -7,15 +8,45 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class SampleController {
 
-    @GetMapping("/order/1")
-    public String getOrder() {
-        log.info("Get some order");   // log는 Slf4j에 속함
-        return "orderId:1, orderAmount:1000";
+    @GetMapping("/order/{orderId}")
+    public String getOrder(@PathVariable("orderId") String id) {
+        log.info("Get some order : " + id);   // log는 Slf4j에 속함
+        return "orderId:" + id + ", " + "orderAmount:1000";
+    }
+
+    @DeleteMapping("/order/{orderId}")
+    public String deleteOrder(@PathVariable("orderId") String id) {
+        log.info("Delete some order : " + id);   // log는 Slf4j에 속함
+        return "Delete orderId:" + id;
+    }
+
+    @GetMapping("/order")
+    public String getOrderWithRequestParam(
+            @RequestParam(value = "orderId", required = false, defaultValue = "defaultId") String id,
+            @RequestParam("orderAmount") Integer amount) {
+        log.info("Get order : " + id + ", amount : " + amount);
+        return "orderId:" + id + ", " + "orderAmount:" + amount;
     }
 
     @PostMapping("/order")
+    public String createOrder(
+            @RequestBody CreateOrderRequest createOrderRequest,
+            @RequestHeader String userAccountId) {
+        log.info("Create order : " + createOrderRequest +
+                ", userAccountId : " + userAccountId);
+        return "orderId:" + createOrderRequest.getOrderId() + ", " +
+                "orderAmount:" + createOrderRequest.getOrderAmount();
+    }
+
+    @PutMapping("/order")
     public String createOrder() {
-        log.info("Create some order");   // log는 Slf4j에 속함
+        log.info("Create some order");
         return "order created -> orderId:1, orderAmount:1000";
+    }
+
+    @Data    // lombok - getter/setter/생성자를 줄여줌
+    public static class CreateOrderRequest {
+        private String orderId;
+        private Integer orderAmount;
     }
 }
