88c077c1b22430f1b99ee418f378c3edbb114d99
Ogu1208 <kdasunb@naver.com>
2022-11-30 21:27:17 +0900

    축약형 매핑 어노테이션 : @GetMapping, @PostMapping -> intellij http test를 통해 http 요청 테스트

--- Part 07 스프링 프레임워크/WebSample/src/main/java/com/example/websample/controller/SampleController.java
+++ Part 07 스프링 프레임워크/WebSample/src/main/java/com/example/websample/controller/SampleController.java
@@ -1,17 +1,21 @@
 package com.example.websample.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j   // lombok 라이브러리
 @RestController
 public class SampleController {
 
-    @RequestMapping(value = "/order/1", method = RequestMethod.GET)
+    @GetMapping("/order/1")
     public String getOrder() {
         log.info("Get some order");   // log는 Slf4j에 속함
         return "orderId:1, orderAmount:1000";
+    }
+
+    @PostMapping("/order")
+    public String createOrder() {
+        log.info("Create some order");   // log는 Slf4j에 속함
+        return "order created -> orderId:1, orderAmount:1000";
     }
 }
