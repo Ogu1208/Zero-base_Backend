cb322127a2f08712cf9295a2764e40baa8bf1f97
Ogu1208 <kdasunb@naver.com>
2022-12-16 20:28:26 +0900

    계좌 확인 API Test: AccountService, AccountController 테스트 fromEntity -> getBalance에 의해 builder에 balance도 초기화 시켜야함

--- Projects/계좌 시스템/src/main/java/com/example/account/dto/AccountDto.java
+++ Projects/계좌 시스템/src/main/java/com/example/account/dto/AccountDto.java
@@ -24,6 +24,7 @@ public class AccountDto {
         return AccountDto.builder()
                 .userId(account.getAccountUser().getId())
                 .accountNumber(account.getAccountNumber())
+                .balance(account.getBalance())
                 .registeredAt(account.getRegisteredAt())
                 .unregisteredAt(account.getUnregisteredAt())
                 .build();
